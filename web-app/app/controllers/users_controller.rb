049ae275437fc3831eac4b07c19ec53de15fa843
Sulik Khavazov <good.sulik@gmail.com>
2017-11-09 15:20:08 +0600

    №5. Созданы контроллеры для модели User.
    Контроллеры index, new, edit, create, update и destroy.

---
+++ web-app/app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+  def new
+  	@user = User.new
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def index
+  	@users = User.all
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to root_path
+  		flash[:success] = "User successfuly created."
+  	else
+  		render "new"
+  		flash[:danger] = "Error."
+  	end
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		redirect_to root_path
+  		flash[:success] = "User successfuly updated"
+  	else
+  		render "edit"
+  		flash[:danger] = "Error."
+  	end
+  end
+
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+
+  	redirect_to root_path
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password)
+  end
+end
--- web-app/config/routes.rb
+++ web-app/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/index'
+
   devise_for :users
   root 'users#index'
 end
