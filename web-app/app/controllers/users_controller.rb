0fec45a053e6b29dc892a8ab24713ed2e0e3e9a8
Sulik Khavazov <good.sulik@gmail.com>
2017-11-09 16:36:45 +0600

    №10. Добавлена возможность отправлять ссылки.
    Теперь возможно отправлять ссылки с токенами на имэйл с функцией подтверждения. Удалена картинка lolface.

--- web-app/app/controllers/users_controller.rb
+++ web-app/app/controllers/users_controller.rb
@@ -11,6 +11,10 @@ class UsersController < ApplicationController
   	@users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
   	@user = User.new(user_params)
   	if @user.save
