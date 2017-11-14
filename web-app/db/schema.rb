e55392517e53b9c3ebdab1f0aa57394189724db3
Andrei Nishanov <mazahaker93@gmail.com>
2017-11-14 14:59:27 +0600

    Добавил таблицу roles в БД

--- web-app/db/schema.rb
+++ web-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114085315) do
+ActiveRecord::Schema.define(version: 20171114085713) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 20171114085315) do
     t.datetime "updated_at", null: false
     t.integer "category_id"
     t.index ["category_id"], name: "index_messages_on_category_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "title", null: false
+    t.text "description", null: false
+    t.text "the_role", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
