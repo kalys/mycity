34eb92328764bbc1fd35a4cc4a0f74fca594b334
Andrei Nishanov <mazahaker93@gmail.com>
2017-11-15 18:57:17 +0600

    #14 Настроен доступ к странице продолжения регистрации

--- web-app/app/controllers/invitations_controller.rb
+++ web-app/app/controllers/invitations_controller.rb
@@ -1,3 +1,3 @@
 class InvitationsController < Devise::InvitationsController
-  before_action :role_required # проверяет роль юзера
+  before_action :role_required, except: :edit # проверяет роль юзера
 end
