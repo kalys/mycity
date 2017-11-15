f50a44ccea5e68e249355905b07193a699d169a6
Andrei Nishanov <mazahaker93@gmail.com>
2017-11-15 15:36:40 +0600

    Добавлена таблица categories

--- web-app/db/schema.rb
+++ web-app/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20171113111736) do
-=======
 ActiveRecord::Schema.define(version: 20171114085713) do
->>>>>>> access_rights
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
