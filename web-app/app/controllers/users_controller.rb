6662950387eb9cdbeebc60bebef8845da1da4f54
Andrei Nishanov <mazahaker93@gmail.com>
2017-11-15 15:58:53 +0600

    #14 Добавил поле role_id в permit параметры

--- web-app/app/controllers/users_controller.rb
+++ web-app/app/controllers/users_controller.rb
@@ -50,6 +50,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-  	params.require(:user).permit(:name, :email, :password)
+  	params.require(:user).permit(:name, :email, :password, :role_id)
   end
 end
