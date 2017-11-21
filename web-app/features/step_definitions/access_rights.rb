c7debaf1448f4979c606ec3e3aef509806bf6588
Andrei Nishanov <mazahaker93@gmail.com>
2017-11-21 16:39:33 +0600

    Написал тесты для прав доступа

--- web-app/features/step_definitions/access_rights.rb
+++ web-app/features/step_definitions/access_rights.rb
@@ -24,31 +24,36 @@ When(/^появляется надпись "([^"]*)"$/) do |title|
 end
 
 
-When(/^администратор находится в управлении группой "([^"]*)"$/) do |title|
-	visit('/users/sign_in')
-	within('#new_user') do 
-		fill_in('Email', with: 'admin@admin.ru')
-		fill_in('Password', with: 'qweqweqwe')
-		click_button('Log in')
-	end
+When(/^администратор находится в управлении группой модераторы$/) do 
 	visit('/admin/roles/2/edit')	
-	sleep 3
 end
 
 When(/^он добавит доступ к странице "([^"]*)"$/) do |title|
-	sleep 3
 	within(:xpath, "//form[@id='new_section']") do 
 		fill_in('section_name', with: title)
-		sleep 3
 		click_button('Create new section')
-		sleep 3
 	end
 end
 
 When(/^появится надпись "([^"]*)"$/) do |title|
-	sleep 3
 	page.has_xpath?("//*[@id='toast-container']//div[contains(text(), '#{title}')]")
-	sleep 3
+end
+
+When (/^залогинен пользователь с email "([^"]*)" и паролем "([^"]*)" и он попытается зайти в админку$/) do |email, password|
+	visit(root_path)
+	click_link('Logout')
+	visit('/users/sign_in')
+	within('#new_user') do 
+		fill_in('Email', with: email)
+		fill_in('Password', with: password)
+		click_button('Log in')
+		sleep 3
+	end
+	visit('/admin/roles')
+end
+
+When(/^его переадресует на страницу с ошибкой "([^"]*)"$/) do |error_title|
+	page.has_xpath?("//h1[contains(text(), '#{error_title}')]")
 end
 
 		# Допустим администратор заходит в управление группой "тест"
