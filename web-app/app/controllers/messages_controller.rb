61fe146a5f2eb54b3a868b96e1c8f38f821c264e
Sulik <good.sulik@gmail.com>
2017-11-25 17:25:49 +0600

    #24 Сортировка по статусу
    Тыкаешь на кнопку с названием и - о боги! Они отсортированы!

--- web-app/app/controllers/messages_controller.rb
+++ web-app/app/controllers/messages_controller.rb
@@ -4,7 +4,8 @@ class MessagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:create]
 
   def index
-    @messages = Message.where.not(status: :hidden).order(created_at: :desc)
+    @status = params[:status] if !params[:status].nil?
+    @messages = Message.all.order(created_at: :desc)
     @hash = Gmaps4rails.build_markers(@messages) do |message, marker|
       marker.lat message.latitude
       marker.lng message.longitude
@@ -58,4 +59,5 @@ class MessagesController < ApplicationController
   def image_params
     params.require(:image).permit(:image)
   end
+
 end
