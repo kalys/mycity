f7fc61fc7912f6dc9021973ebe4a16c1717cc151
Sultan <bekovs96@gmail.com>
2017-12-01 15:14:09 +0600

    #30 Добавил разделение сообщений по цвету (статус)

--- web-app/app/helpers/messages_helper.rb
+++ web-app/app/helpers/messages_helper.rb
@@ -1,2 +1,16 @@
 module MessagesHelper
+	def panel_type(message)
+		case message.status
+		when "new_message"
+			return "info"
+		when "actual"
+			return "warning"
+		when "done"
+			return "success"
+		when "not_relevant"
+			return "default"
+		else
+			return "primary"
+		end
+	end
 end
