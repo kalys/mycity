330cebac7e7fe2473f17fff0dd48754ccca0ea84
Japarov Almaz <almazmadmax@gmail.com>
2017-12-11 15:15:19 +0600

    #27 Добавил простенькую валидацию на приходящие данные.

--- telegram-bot/main.rb
+++ telegram-bot/main.rb
@@ -5,7 +5,7 @@ require './session.rb'
 
 class FixMyStreet
 	def initialize
-		@TOKEN     = '499660598:AAF-INPO3WiMq2DfO7KFLyfgMrUlpi9RvVQ'
+		@TOKEN     = '500989121:AAFjlkE097YZkyEe9F6jqB8rq0AObyU0Gr0'
 		@sessions  = []
 		@session   = nil
 	end
@@ -15,59 +15,69 @@ class FixMyStreet
 			bot.listen do |message|
 				current_chat = message.chat.id
 
-				check_session(current_chat)			
-				
+				check_session(current_chat)
+
 				if message.text == '/start'
 					bot.api.send_message(chat_id: message.chat.id, text: 'Здрасте, Вы можете мне отправить проблему написав /new')
 				elsif message.text == '/new'
 					@session.status = '/new'
-				elsif message.text == '/end' && @session.status != nil			
+				elsif message.text == '/end' && @session.status != nil
 					@session.send_parameters
 
 					bot.api.send_message(chat_id: current_chat, text: 'Ваше сообщение отправлено на модерацию.')
-				end	
+				end
 
 
-				case @session.status	
+				case @session.status
 				when '/new'
-					kb = Telegram::Bot::Types::KeyboardButton.new(text: 'Show me your location', request_location: true) 
+					kb = Telegram::Bot::Types::KeyboardButton.new(text: 'Show me your location', request_location: true)
 					markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
 					bot.api.send_message(chat_id: current_chat, text: 'Вы можете отправить адрес написав где Вы находитесь, либо отправить ваши координаты, нажав кнопку "Show me your location"', reply_markup: markup)
 					@session.status = 'location'
 				when 'location'
 					unless message.location.nil?
-						@session.latitude = message.location.latitude 
+						@session.latitude = message.location.latitude
 						@session.longitude = message.location.longitude
 						@session.status = 'text'
-						bot.api.send_message(chat_id: current_chat, text: 'Опишите проблему: ')	
-					else 
-						@session.address = message.text
-						@session.status = 'text'
 						bot.api.send_message(chat_id: current_chat, text: 'Опишите проблему: ')
-					end					
-				when 'text'	
-				  @session.text = message.text
-				  @session.status = 'images'
-				  bot.api.send_message(chat_id: current_chat, text: 'Отправте изображения проблемы и когда закончите, напишите /end')
-				when 'images'	
+					else
+						unless message.text.nil?
+							@session.address = message.text
+							@session.status = 'text'
+							bot.api.send_message(chat_id: current_chat, text: 'Опишите проблему: ')
+						else
+							bot.api.send_message(chat_id: current_chat, text: 'Повторите попытку.')
+						end
+					end
+				when 'text'
+				  unless message.text.nil?
+				  	@session.text = message.text
+					  @session.status = 'images'
+					  bot.api.send_message(chat_id: current_chat, text: 'Отправте изображения проблемы и когда закончите, напишите /end')
+				  else
+				  	bot.api.send_message(chat_id: current_chat, text: 'Повторите попытку.')
+				  end
+				when 'images'
 					unless message.photo[0].nil?
 						Dir.mkdir("./pictures/") unless File.exists?("./pictures/")
 						Dir.mkdir("./pictures/#{current_chat}") unless File.exists?("./pictures/#{current_chat}/")
 
-						file = bot.api.get_file(file_id: message.photo[2].file_id) 
+						file = bot.api.get_file(file_id: message.photo[2].file_id)
 						file_path = file.dig('result', 'file_path')
 						photo_url = "https://api.telegram.org/file/bot#{@TOKEN}/#{file_path}"
-						File.write("./pictures/#{current_chat}/image_#{current_chat}.jpg", open(photo_url).read)		
+						File.write("./pictures/#{current_chat}/image_#{current_chat}.jpg", open(photo_url).read)
 
-						path_to_file = "./pictures/#{current_chat}/image_#{current_chat}.jpg" 
+						path_to_file = "./pictures/#{current_chat}/image_#{current_chat}.jpg"
 
 						@session.images.push(File.new(path_to_file, 'rb'))
 
-						FileUtils.rm(path_to_file)		
+						FileUtils.rm(path_to_file)
+					else
+						bot.api.send_message(chat_id: current_chat, text: 'Повторите попытку.')
 					end
 				end
-			end	
-		end		
+			end
+		end
 	end
 
 	private
@@ -78,7 +88,7 @@ class FixMyStreet
 
 		@session = @sessions.find {|session| session.chat_id == chat_id}
 	end
-end	
+end
 
 bot = FixMyStreet.new
 bot.run
--- telegram-bot/session.rb
+++ telegram-bot/session.rb
@@ -2,7 +2,7 @@ require 'rest-client'
 
 class Session
 	attr_reader   :chat_id
-	attr_accessor :status, :text, :images, :text, :address, :latitude, :longitude 
+	attr_accessor :status, :text, :images, :text, :address, :latitude, :longitude
 
 	def initialize(chat_id)
 		@chat_id   = chat_id
@@ -15,20 +15,20 @@ class Session
 	end
 
 	def send_parameters
-		response = RestClient.post('http://188.226.182.111/messages', 
-		{  :message => { 
+		response = RestClient.post('http://localhost:3000/messages',
+		{  :message => {
 				 category_id: 1,
 				 longitude:   @longitude,
 				 latitude:    @latitude,
 				 address:     @address,
-				 status:     'new_message', 
+				 status:     'new_message',
 				 body:        @text },
 		})
 
 		message_id = response.body
 
 		@images.each do |image|
-			RestClient.post("http://188.226.182.111/messages/#{message_id}/image", :image => image)
+			RestClient.post("http://localhost:3000/messages/#{message_id}/image", :image => image)
 		end
 
 		initialize(@chat_id)
