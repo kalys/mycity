5ee6a90d3a9311e189fdefabcfd5bdb93b0c81bc
Unknown <good.sulik@gmail.com>
2017-12-25 17:49:53 +0600

    #45, выполнена сборка Docker'a.
    Теперь в проекте есть docker-compose.yml

--- web-app/config/puma_deploy.rb
+++ web-app/config/puma_deploy.rb
@@ -1,25 +1,10 @@
-# Change to match your CPU core count
 workers 2
-
-# Min and Max threads per worker
 threads 1, 6
-
 app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-
-# Default to production
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
-
-# Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
-
-# Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-
-# Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
+bind "tcp://0.0.0.0:3000"
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 activate_control_app
 
 on_worker_boot do
