9a71ab514d140afc2f6429116b1848d7ba89e441
Andrei Nishanov <mazahaker93@gmail.com>
2017-12-27 15:17:17 +0600

    #29 Вынес сообщения от бота в отдельный метод

--- telegram-bot/telegram_bot.rb
+++ telegram-bot/telegram_bot.rb
@@ -6,6 +6,13 @@ module TelegramBot
 
   # TOKEN = '500989121:AAFjlkE097YZkyEe9F6jqB8rq0AObyU0Gr0'
 TOKEN = '484322269:AAGmC6awc5ZWBev9PYkgEfE1tJjHXbeqvmc'
+BOT_COMMAND = {
+  start_bot: "/start",
+  new_problem: "Сообщить о новой проблеме",
+  send_problem: "Сообщить о проблеме",
+  cancel_problem: "Отменить",
+  send_geolocation: 'Отправить геопозицию'
+}
   class Run
     attr_accessor :session
 
@@ -20,33 +27,26 @@ TOKEN = '484322269:AAGmC6awc5ZWBev9PYkgEfE1tJjHXbeqvmc'
           current_chat = command.chat.id
           check_session(current_chat)
           case command.text
-          when '/start', 'Отмена'
-            bot.api.send_message(chat_id: current_chat,
-                                text: UserMessages.first_greeting_message)
-            bot.api.send_message(chat_id: current_chat,
-                                text: UserMessages.second_greeting_message,
-                                reply_markup: UserMessages.buttons('Сообщить о новой проблеме'))
-          when 'Сообщить о новой проблеме'
+          when BOT_COMMAND[:start_bot], BOT_COMMAND[:cancel_problem]
+            UserMessages.bot_messages(command.text, current_chat)
+          when BOT_COMMAND[:new_problem]
             Location.new(current_chat, @session).get_adress
             @session.check_address = @session.address
-            bot.api.send_message(chat_id: current_chat,
-                                text: UserMessages.information_message,
-                                reply_markup: UserMessages.buttons('Сообщить о проблеме'))
+            UserMessages.bot_messages(command.text, current_chat)
             bot.listen do |message|
-              if message.text == 'Сообщить о проблеме'
+              if message.text == BOT_COMMAND[:send_problem]
                 @session.send_parameters
-                bot.api.send_message(chat_id: current_chat,
-                                    text: UserMessages.success_input,
-                                    reply_markup: UserMessages.buttons('Сообщить о новой проблеме'))
+                UserMessages.bot_messages(message.text, current_chat)
                 break
-              elsif message.text == 'Отмена'
+              elsif message.text == BOT_COMMAND[:cancel_problem]
+                UserMessages.bot_messages(message.text, current_chat)
                 break
               else
                 Message.new(message, current_chat, @session).save_message
               end
             end
           else
-            bot.api.send_message(chat_id: current_chat, text: UserMessages.error)
+            UserMessages.bot_messages(current_chat)
           end
         end
       end
@@ -95,9 +95,7 @@ TOKEN = '484322269:AAGmC6awc5ZWBev9PYkgEfE1tJjHXbeqvmc'
 
     def get_adress
       Telegram::Bot::Client.run(TOKEN) do |bot|
-        bot.api.send_message(chat_id: @current_chat,
-                            text: UserMessages.location,
-                            reply_markup: UserMessages.buttons('Отправить геопозицию', true))
+        UserMessages.bot_messages(BOT_COMMAND[:send_geolocation], @current_chat)
         bot.listen do |location_message|
           next if location_message.text == 'Сообщить о новой проблеме'
           if !location_message.text.nil?
@@ -108,7 +106,7 @@ TOKEN = '484322269:AAGmC6awc5ZWBev9PYkgEfE1tJjHXbeqvmc'
             @session.longitude = location_message.location.longitude
             break
           else
-            bot.api.send_message(chat_id: @current_chat, text: UserMessages.error)
+            UserMessages.bot_messages(@current_chat)
           end
         end
       end
@@ -182,12 +180,37 @@ TOKEN = '484322269:AAGmC6awc5ZWBev9PYkgEfE1tJjHXbeqvmc'
 
       def buttons(button_text, boolean=false)
         first_button = Telegram::Bot::Types::KeyboardButton.new(text: button_text, request_location: boolean)
-        second_button = Telegram::Bot::Types::KeyboardButton.new(text: 'Отмена')
+        second_button = Telegram::Bot::Types::KeyboardButton.new(text: BOT_COMMAND[:cancel_problem])
 
-        if button_text == 'Сообщить о проблеме'
+        if button_text == BOT_COMMAND[:send_problem]
           markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [first_button, second_button], resize_keyboard: true)
         else
           markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [first_button], resize_keyboard: true)
+        end
+      end
+
+      def bot_messages(message=nil, current_chat)
+        Telegram::Bot::Client.run(TOKEN) do |bot|
+          case message
+          when BOT_COMMAND[:start_bot], BOT_COMMAND[:cancel_problem]
+            bot.api.send_message(chat_id: current_chat, text: first_greeting_message)
+            bot.api.send_message(chat_id: current_chat, text: second_greeting_message,
+                                reply_markup: buttons(BOT_COMMAND[:new_problem]))
+          when BOT_COMMAND[:new_problem]
+            bot.api.send_message(chat_id: current_chat,
+                                text: information_message,
+                                reply_markup: buttons(BOT_COMMAND[:send_problem]))
+          when BOT_COMMAND[:send_problem]
+            bot.api.send_message(chat_id: current_chat,
+                                text: success_input,
+                                reply_markup: buttons(BOT_COMMAND[:new_problem]))
+          when BOT_COMMAND[:send_geolocation]
+            bot.api.send_message(chat_id: current_chat,
+                                text: location,
+                                reply_markup: buttons(BOT_COMMAND[:send_geolocation], true))
+          else
+            bot.api.send_message(chat_id: current_chat, text: error)
+          end
         end
       end
     end
