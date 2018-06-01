7a5b18458cb4ea4bdb16b9adddbd6849cdec4ebe
Kalys Osmonov <kalys@osmonov.com>
2018-06-01 17:12:09 +0600

    Set env in dockerfile; Remove config from dev env

--- web-app/config/environments/development.rb
+++ web-app/config/environments/development.rb
@@ -34,7 +34,5 @@ Rails.application.configure do
 
   config.assets.quiet = true
 
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
   config.active_job.queue_adapter = :sidekiq
 end
